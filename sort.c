17686da3cc16f26f575deff51082c0409b934757
alenkatopproger <saaamsi58@gmail.com>
2025-03-16 21:46:21 +0700

    f

--- sort.c
+++ sort.c
@@ -1,11 +1,21 @@
-for (int i = 0; i < n-1; i++) {        
-        for (int j = 0; j < n-i-1; j++) {   
-            if (array[j] > array[j+1]) {   
-                 int temp = array[j];
-                 array[j] = array[j+1]
-                 array[j+1] = temp
-            }
+#include <stdio.h>
+
+int main() {
+        int n=2;
+        int array[n]={4,0};
+                for (int i = 0; i < n-1; i++) {
+                for (int j = 0; j < n-i-1; j++) {
+                if (array[j] > array[j+1]) {
+                        int temp = array[j];
+                        array[j] = array[j+1]
+                        array[j+1] = temp
+                }
+                }
+        for (int i=0; i<n; i++)
+                printf(" %d", array[i]);
+        return 0;
         }
-    }
+}
 //комментарий для пункта9
 //комментарий для пункта13
+
